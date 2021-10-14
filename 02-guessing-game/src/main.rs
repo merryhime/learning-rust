404ad5840fd9bbe7ee8a291a832560e00a6e870f
Merry <git@mary.rs>
2021-10-14 12:47:34 +0100

    02: Guessing Game

--- 02-guessing-game/src/main.rs
+++ 02-guessing-game/src/main.rs
@@ -1,11 +1,23 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Input guess.");
 
-    let mut guess = String::new();
+    let secret = rand::thread_rng().gen_range(1..101);
+    // println!("Secret: {}", secret);
 
+    let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Fail to read line");
+    let guess: u32 = guess.trim().parse().expect("Expected number");
 
     println!("Guess: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win"),
+    }
+
 }
