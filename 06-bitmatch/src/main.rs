d62a9a84cb80d6d955e021f8e7c6dd2f7ee8a2d1
Merry <git@mary.rs>
2022-09-27 17:58:18 +0100

    06: bitpack!

--- 06-bitmatch/src/main.rs
+++ 06-bitmatch/src/main.rs
@@ -1,6 +1,25 @@
 use bitmatch::bitmatch;
 
 #[bitmatch]
+pub fn unpack_instructions(bytes: std::vec::Vec<u8>) -> Option<[u32; 4]> {
+    if let &[a, b, cd, e, fg, h, ij, k, l] = &bytes[..9] {
+        let c = (cd & 0b11000000) >> 6;
+        let d = cd & 0b00111111;
+        let f = (fg & 0b11110000) >> 4;
+        let g = fg & 0b00001111;
+        let i = (ij & 0b11111100) >> 2;
+        let j = ij & 0b00000011;
+        let ins1: u32 = bitpack!("aaaaaaaabbbbbbbbcc");
+        let ins2: u32 = bitpack!("ddddddeeeeeeeeffff");
+        let ins3: u32 = bitpack!("gggghhhhhhhhiiiiii");
+        let ins4: u32 = bitpack!("jjkkkkkkkkllllllll");
+        Some([ins1, ins2, ins3, ins4])
+    } else {
+        None
+    }
+}
+
+#[bitmatch]
 fn decode(inst: u8) -> String {
     #[bitmatch]
     match inst {
@@ -14,4 +33,8 @@ fn decode(inst: u8) -> String {
 
 fn main() {
     println!("{}\n", decode(0xf7));
+    println!(
+        "{:?}\n",
+        unpack_instructions(vec![0, 1, 2, 3, 4, 5, 6, 7, 8])
+    );
 }
